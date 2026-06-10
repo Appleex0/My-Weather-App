4e753911c232bfdf1a0d0a941a97557b2cdab9fe
Appleex0 <agakhalilov@outlook.com>
2026-06-10 15:48:05 +0400

    feat: update Footer component with copyright and developer information

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,22 @@
 function Footer() {
-  return <div className="bg-gray-400 w-full h-20">Footer</div>;
+  return (
+    <div className="bg-gray-400 w-full h-20">
+      <div className="flex justify-around items-center h-full">
+        <p className="text-white text-lg">
+          © 2026 Weather App. All rights reserved.
+        </p>
+        <p className="text-white text-lg ">
+          Developed by{" "}
+          <a
+            href="mailto:your.email@example.com"
+            className="text-blue-500 font-bold hover:text-blue-700"
+          >
+            Khalilov Dev
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
